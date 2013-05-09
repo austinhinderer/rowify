0f1dc348277c237d203975d2a4b3b851eb96588a
Austin Hinderer <me@austinhinderer.com>
2013-05-09 00:54:13 -0700

    Updated comments to contain correct license

--- rowify.js
+++ rowify.js
@@ -1,29 +1,25 @@
 //////////////////////////////////////////////////////
-// Rowify written by Austin Hinderer
-// Equalizes column heights within a container
-//
-//  Usage:
-//  Add the 'data-same-height' data attribute to
-//  a container with columns you want to equalize.
-//  Add the $(document).ready() and
-//  $(window).resize() javascript after the site JS.
-//
-//  Example:
-//  <div data-same-height>
-//    <div class="column">Short</div>
-//    <div class="column">Long column</div>
-//    <div class="column">Short</div>
-//  </div>
-//
-//  <script>
-//    $(document).ready(function(){
-//      $('[data-same-height]').rowify();
-//    });
-//
-//    $(window).resize(function() {
-//      $('[data-same-height]').rowify();
-//    });
-//  </script>
+// The MIT License (MIT)
+// 
+// Copyright (c) 2013 Austin Hinderer
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 //
 //////////////////////////////////////////////////////
 
